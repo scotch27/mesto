fe0a5cd16db69636cec7d5da49eba7db3d6b0ee9
scotch <scotch27@mail.ru>
2023-02-05 00:54:35 +0300

    добавлена обработка событий

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+const editButton = document.querySelector('.profile__edit-button_type_edit');
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.popup__close-button')
+
+const toggleOpenPopup = function (){
+    popup.classList.toggle('popup_opened');
+}
+
+const handleEditButtonClick = function (){
+    toggleOpenPopup();
+}
+
+const handleCloseButtonClick = function (){
+    toggleOpenPopup();
+}
+
+const handleOverlayButtonClick = function (event){
+    if (event.target === event.currentTarget){
+        toggleOpenPopup();
+    }
+    };
+
+editButton.addEventListener('click', handleEditButtonClick);
+closeButton.addEventListener('click', handleEditButtonClick);
+popup.addEventListener('click', handleOverlayButtonClick);
